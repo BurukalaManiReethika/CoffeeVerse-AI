5ad9ed49a59a97a6d865132684e5632fc76fe248
BURUKALA MANI REETHIKA <manireethikab@gmail.com>
2026-06-13 17:35:23 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -107,3 +107,17 @@ loader.style.display="none";
 });
 
 }
+const cursor =
+document.querySelector(".cursor");
+
+document.addEventListener(
+"mousemove",
+e=>{
+
+cursor.style.left =
+e.clientX + "px";
+
+cursor.style.top =
+e.clientY + "px";
+
+});
