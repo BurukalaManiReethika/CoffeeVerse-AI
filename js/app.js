cd1e1c93c3ff2ba66fa7316b3d12d654da3ac82f
BURUKALA MANI REETHIKA <manireethikab@gmail.com>
2026-06-13 18:06:32 +0530

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", () => {
+
+    const loader = document.querySelector(".loader");
+
+    if (loader) {
+        loader.style.display = "none";
+    }
+
+    console.log("App Loaded Successfully");
+
+});
